03410a909fcb11b35352b6604b6bfd2a9923bb8a
Gaegeumchi <gaegeumchi@gmail.com>
2025-05-25 17:05:26 +0900

    Update First_Person.cs

--- Character Controller/First_Person.cs
+++ Character Controller/First_Person.cs
@@ -1,26 +1,35 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class First_PersonController : MonoBehaviour
 {
-    public float mouseSensitivity = 400f;
-    public float moveSpeed = 7f;
+    public float mouseSensitivity = 80f;
+    public float moveSpeed = 10f;
     public float jumpForce = 7f;
     public Camera playerCamera;
+    public bool Sprint = false;
+    public float SprintSpeed = 15f;
 
     private Rigidbody rb;
     private float xRotation = 0f;
     private bool isGrounded = false;
+    private bool isSprinting = false;
+
+    private PlayerInput playerInput;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
+
+        playerInput = GetComponent<PlayerInput>();
     }
 
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        Vector2 mouseDelta = playerInput.actions["Look"].ReadValue<Vector2>();
+        float mouseX = mouseDelta.x * mouseSensitivity * Time.deltaTime;
+        float mouseY = mouseDelta.y * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
@@ -28,33 +37,35 @@ public class First_PersonController : MonoBehaviour
         playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         transform.Rotate(Vector3.up * mouseX);
 
-        // 점프 처리
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (playerInput.actions["Jump"].WasPressedThisFrame() && isGrounded)
         {
-            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
         }
+        
+        isSprinting = Sprint && playerInput.actions["Sprint"].IsPressed();
     }
 
     void FixedUpdate()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
+        Vector2 moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
+        float x = moveInput.x;
+        float z = moveInput.y;
+        
+        float currentSpeed = isSprinting ? SprintSpeed : moveSpeed;
+        
         Vector3 move = transform.right * x + transform.forward * z;
-        Vector3 velocity = new Vector3(move.x * moveSpeed, rb.velocity.y, move.z * moveSpeed);
+        Vector3 velocity = new Vector3(move.x * currentSpeed, rb.linearVelocity.y, move.z * currentSpeed);
 
-        rb.velocity = velocity;
+        rb.linearVelocity = velocity;
     }
 
     private void OnTriggerStay(Collider collision)
     {
-        if (collision.CompareTag("Ground"))
-            isGrounded = true;
+        isGrounded = true;
     }
 
     private void OnTriggerExit(Collider collision)
     {
-        if (collision.CompareTag("Ground"))
-            isGrounded = false;
+        isGrounded = false;
     }
 }
